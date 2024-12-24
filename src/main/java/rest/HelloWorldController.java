03fdde546ca166f73de3cf8f39e2f9793822ad3a
VictorSordi <victorsordi200301@outlook.com>
2024-12-24 11:35:51 -0300

    Alterado pasta src

--- src/main/java/rest/HelloWorldController.java
+++ src/main/java/rest/HelloWorldController.java
@@ -2,10 +2,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Teste {
+public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String helloWorld() {
-        return "Hello";
+        return "Hello, World!";
     }
 }
