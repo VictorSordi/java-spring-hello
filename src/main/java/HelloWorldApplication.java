4a18e55e42e23126e92ae7a4a4f722999af52952
VictorSordi <victorsordi200301@outlook.com>
2024-12-24 11:43:20 -0300

    Alterado pasta src

--- src/main/java/HelloWorldApplication.java
+++ src/main/java/HelloWorldApplication.java
@@ -2,7 +2,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
