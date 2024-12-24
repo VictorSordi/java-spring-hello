c512fc525cc48c5ff46ffdf73d2d1e7d7893279a
VictorSordi <victorsordi200301@outlook.com>
2024-12-24 17:13:46 -0300

    Alterado pasta src  para code coverage no sonar

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -1,11 +1,16 @@
 package com.example.helloworld;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class HelloWorldApplicationTests {
+class HelloWorldApplicationTests {
 
     @Test
-    public void main() {
+    void testMainMethod() {
+        // Chama o método main para garantir que a aplicação inicialize sem erros
         HelloWorldApplication.main(new String[] {});
-   }
+
+        // Se o Spring Boot iniciar corretamente, o teste não falhará. Para uma verificação mais explícita,
+        // você pode adicionar alguma lógica aqui, como verificar a presença de um bean específico no contexto.
+        // No caso, estamos assumindo que o método main iniciou o contexto do Spring corretamente.
+    }
 }
