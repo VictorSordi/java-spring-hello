ffe46b1b51ebad847fc1909ec7b111d9ea9e57cd
VictorSordi <victorsordi200301@outlook.com>
2024-12-24 13:03:00 -0300

    Alterado pasta src

--- src/test/java/com/example/helloworld/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/HelloWorldControllerTest.java
@@ -1,26 +1,21 @@
-package com.example.helloworld;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloWorldControllerTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
+class YourTestClass {
 
     @Test
-    public void helloWorldTest() {
+    private void testGetRequest() {
+        // Cria uma instância do RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
         // Realiza uma solicitação GET para o endpoint "/"
         ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 
         // Verifica se o status da resposta é 200 (OK) e se o corpo da resposta é "Hello, World!"
-        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("Hello, World!", response.getBody());
     }
 }
