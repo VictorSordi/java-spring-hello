d0bef33232713bde18e0b35589a97d2151e7f822
VictorSordi <victorsordi200301@outlook.com>
2024-12-24 16:59:46 -0300

    Alterado pasta src  para code coverage no sonar

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class HelloWorldApplicationTests {
